7a96ffeaeb78cdc5ca17c5190eff46b25f8afb77
Rody Huancas <rodyhuancas.04@gmail.com>
2024-10-07 19:34:27 -0500

    Feat: chart donut

--- src/reports/basic-chart-svg.report.ts
+++ src/reports/basic-chart-svg.report.ts
@@ -25,8 +25,38 @@ const generateChartImage = async () => {
   return Utils.chartJsToImage(chartConfig);
 };
 
+const generateDonut = async () => {
+  const DATA_COUNT = 5;
+  const NUMBER_CFG = { count: DATA_COUNT, min: 0, max: 100 };
+
+  const data = {
+    labels: ['Red', 'Orange', 'Yellow', 'Green', 'Blue'],
+    datasets: [
+      {
+        label: 'Dataset 1',
+        data: Utils.numbers(NUMBER_CFG),
+        backgroundColor: Object.values(Utils.CHART_COLORS),
+      },
+    ],
+  };
+
+  const config = {
+    type: 'doughnut',
+    data: data,
+    options: {
+      title: {
+        display: true,
+        text: 'Chart.js Doughnut Chart',
+      },
+    },
+  };
+
+  return Utils.chartJsToImage(config);
+};
+
 export const getBasicSvgReport = async (): Promise<TDocumentDefinitions> => {
-  const chart = await generateChartImage();
+
+  const [ chart, chartDonut ] = await Promise.all([generateChartImage(), generateDonut()])
 
   return {
     content: [
@@ -37,6 +67,10 @@ export const getBasicSvgReport = async (): Promise<TDocumentDefinitions> => {
       },
       {
         image: chart,
+        width: 500,
+      },
+      {
+        image: chartDonut,
         width: 500,
       },
     ],
